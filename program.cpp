5d2560b1f7d08ccb4bf9ea80ed6f13a4788ecf89
Ahmed <ahmelsayed455@gmail.com>
2025-04-28 23:09:34 +0300

    establishing a c++ program

---
+++ program.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int num;
+    cout << "please, enter a number : ";
+    cin >> num;
+    cout << "Your number is : " << num << endl;
+    return 0;
+}
